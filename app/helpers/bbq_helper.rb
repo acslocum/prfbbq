04bdcd0dcf1907696f76cac6993c23cfe3b7404a
acslocum <ThoughtWorks@acslocum-2.local>
2010-10-10 11:27:42 -0500

    bands are grouped dynamically by day

--- app/helpers/bbq_helper.rb
+++ app/helpers/bbq_helper.rb
@@ -1,2 +1,10 @@
 module BbqHelper
+  def bbq_days(bbq)
+    (bbq.bands.collect {|band| band.play_date}).uniq
+  end
+  
+  def bands_for_day(date, bbq)
+    bbq.bands.select { |band| band.play_date == date}
+  end
+  
 end
