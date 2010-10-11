ad127611698fca1ab7d729ccb9ad35984d187669
Mat Biscan <mdbiscan@gmail.com>
2010-10-10 22:19:35 -0500

    updated all admin templates - addAMPM updates

--- public/javascripts/addAMPM.js
+++ public/javascripts/addAMPM.js
@@ -4,23 +4,9 @@ function AddAMPM(input) {
       offset = $(input).offset();
       
   var am = item.clone().text('AM').appendTo(ampm).css({'cursor':'pointer'}),
-      pm = item.clone().text('PM').appendTo(ampm).css({'cursor':'pointer'});
+      pm = item.clone().text('PM').appendTo(ampm).css({'cursor':'pointer'}).addClass('selected');
       
-  var items = ampm.children();
-  items.click(function() {
-    var text = $(this).text(),
-        value = $(input).val().split(' '),
-        newValue = value[0] + ' ' + text;         
-    
-    if(value != '') {
-      $(input).val(newValue);
-      items.removeClass('selected');
-      $(this).addClass('selected');
-    } else {
-      $(input).focus();
-    }
-  });
-    
+  
   ampm
     .css({
       left:offset.left  + $(input).innerWidth(),
@@ -30,4 +16,34 @@ function AddAMPM(input) {
     .appendTo('body')
     .hide()
     .fadeIn(150);
+  
+  var items = ampm.children();
+  items.click(function() {
+    var text = $(this).text(),
+        value = $(input).val().split(' '),
+        newValue = value[0] + ' ' + text;         
+
+    if(value != '') {
+      $(input).val(newValue);
+      items.removeClass('selected');
+      $(this).addClass('selected');
+    } else {
+      $(input).focus();
+    }
+  });
+  
+  $(input).blur(function() {
+    var value = $(this).val(),
+        valueArray = value.split('');
+        
+    if(value != '') {
+      items.filter('.selected').click();
+    }
+    
+    if(valueArray[0] == 0) {
+      var newValue = valueArray[1] + valueArray[2] + valueArray[3] + valueArray[4];
+      $(this).val(newValue);
+      items.filter('.selected').click();
+    }
+  });
 };
