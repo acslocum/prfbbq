eaa5b5f4a39131e39e44f4876743422a424dedad
acslocum <ThoughtWorks@acslocum-2.local>
2010-10-20 16:38:18 -0500

    sorting bands by realistic start time (1 am is after 11 pm)

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,3 +1,31 @@
 class Band < ActiveRecord::Base
   belongs_to :day
+  
+  def <=>(otherBand)
+    #date_compare = day.bbq_date <=> otherBand.day.bbq_date
+    #return date_compare unless date_compare == 0
+    
+    time_cmp = time_compare(start_time, otherBand.start_time)
+    return time_cmp unless time_cmp == 0
+    
+    return id <=> otherBand.id
+  end
+  
+  def time_compare(time, otherTime)
+    now_as_string = Date.today().strftime()
+    dateTime = DateTime.strptime((now_as_string+"T"+time),"%FT%l:%M %p")
+    otherDateTime = DateTime.strptime((now_as_string+"T"+otherTime),"%FT%l:%M %p")
+    dateTime += sort_factor(dateTime)
+    otherDateTime += sort_factor(otherDateTime)
+    return dateTime <=> otherDateTime
+  end
+  
+  def sort_factor(dateTime)
+    if(dateTime.hour < 5)
+      1
+    else
+      0
+    end
+  end
+  
 end
