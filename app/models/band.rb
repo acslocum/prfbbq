eacc6e04be35a35ea6895d38392961786e8041fe
acslocum <ThoughtWorks@acslocum-2.local>
2010-10-20 17:03:17 -0500

    adding comment to describe weird band sorting

--- app/models/band.rb
+++ app/models/band.rb
@@ -11,6 +11,8 @@ class Band < ActiveRecord::Base
     return id <=> otherBand.id
   end
   
+  # sort bands by start time, such that "saturday morning 11 am" < "saturday night 10pm" < "saturday night 1am"
+  # implemented as "everything before 5am is tonight. everything after 5 am is tomorrow morning"
   def time_compare(time, otherTime)
     now_as_string = Date.today().strftime()
     dateTime = DateTime.strptime((now_as_string+"T"+time),"%FT%l:%M %p")
